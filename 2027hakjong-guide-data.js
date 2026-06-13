ddc0d04453d691f81109bbaebcd4035fb07c9bcd
esteacher2026 <esteacher2026@users.noreply.github.com>
2026-06-13 19:05:35 +0900

    Add 2027 hakjong guide download page

---
+++ 2027hakjong-guide-data.js
@@ -0,0 +1,21 @@
+// 2027 학생부종합전형 가이드북 원문 다운로드 데이터
+window.HAKJONG_GUIDES_2027 = [
+  { region: "서울", university: "동국대", guideType: "학생부전형", id: "1aAw4Df-fCIy2vgw_4p_JJaMwZ9clILao", title: "2027 동국대 학생부전형가이드북단면.pdf", type: "PDF" },
+  { region: "서울", university: "성신여대", guideType: "농어촌 안내", id: "1cfm0OMs3dR1QVayfnTGX9TcMyHSPnQ09", title: "성신여대 2027학년도 농·어촌 입학정보안내서.pdf", type: "PDF" },
+  { region: "서울", university: "서울시립대", guideType: "학생부종합전형", id: "1J2kuJWptIMesacOGV3iDmLODSnv8Wtua", title: "2026 서울시립대 학생부종합전형 가이드북(2027학년도).pdf", type: "PDF" },
+  { region: "서울", university: "성신여대", guideType: "학생부위주전형", id: "1F_u8COghKmRUO4oR-O-0GK8p7iqCvQmn", title: "성신여자대학교_2027학생부위주전형_가이드북.pdf", type: "PDF" },
+  { region: "호남", university: "전주대", guideType: "학생부종합전형", id: "15qFTkJmgcAFFtGlufTMy4-UsVrxg_ctY", title: "2027 전주대 학생부종합전형 안내.pdf", type: "PDF" },
+  { region: "서울", university: "경희대", guideType: "학생부종합전형", id: "1PAJOhE9f_5M06gF43XrbMVVpX2wF4PP2", title: "2027 경희대 학생부종합 가이드북.pdf", type: "PDF" },
+  { region: "강원", university: "강원대", guideType: "학생부위주전형", id: "1SxWcGNh25znOdhGer_ic81PMTEr_MtA1", title: "2027 강원대(춘천, 삼척) 학생부위주전형 준비 가이드북.pdf", type: "PDF" },
+  { region: "경기·인천", university: "인하대", guideType: "학생부종합전형", id: "1sZFuW_6bwRlWGkhTvSzk5K_9twr_ASbK", title: "2027 인하대 학생부종합전형 가이드북.pdf", type: "PDF" },
+  { region: "서울", university: "한국외대", guideType: "학생부전형", id: "18dgEO20bpcGf_O9pv_70EZ7XJDCPUlMA", title: "2027 한국외대 학생부전형 가이드북.pdf", type: "PDF" },
+  { region: "서울", university: "연세대", guideType: "학생부종합전형", id: "1ykOAEfqHUJc99Vw2cfrfrdNQ94e1HSXa", title: "2027 연세대 학생부종합전형 안내서.pdf", type: "PDF" },
+  { region: "서울", university: "홍익대", guideType: "학생부종합전형", id: "1akvvDk3cJaetk8g9kOBv4FvTPpgwLlW9", title: "2027 홍익대 학생부종합전형가이드북.pdf", type: "PDF" },
+  { region: "충청", university: "한국기술교육대", guideType: "학생부종합전형", id: "1mN31KRBMwLv3MLjRPymk_UW2WLxRqRPR", title: "2027 한국기술교육대 학생부종합전형 가이드북(260313).pdf", type: "PDF" },
+  { region: "서울", university: "서울대", guideType: "학생부종합전형", id: "1yJoJmT-SC4FbKH54XYI3N2IXYeD2oLYj", title: "2027 서울대학교 학생부종합전형 안내.pdf", type: "PDF" },
+  { region: "서울", university: "이화여대", guideType: "학생부위주전형", id: "1P5B5n570WBvJLyU14cGP6c7oz26oi_vp", title: "2027 이화여대 학생부위주전형 안내서.pdf", type: "PDF" },
+  { region: "서울", university: "홍익대", guideType: "미술계열전형", id: "13i8NCVlrPGFhgxvAWZUYNBRUWyCKsqmH", title: "2027 홍익대 미술계열전형 가이드북.pdf", type: "PDF" },
+  { region: "경기·인천", university: "한양대(ERICA)", guideType: "학생부종합전형", id: "1PB2rg0MDFeZNX0bZTPrpmQwIBGtdBkpp", title: "2027 한양대(에리카) 학생부종합전형 가이드북.pdf", type: "PDF" },
+  { region: "서울", university: "국민대", guideType: "학생부전형", id: "1VDWqnLfOACPH4UwPJlj8gssA_-V8aqid", title: "2027_국민대_학생부전형_가이드(단면).pdf", type: "PDF" },
+  { region: "서울", university: "숭실대", guideType: "학생부종합전형", id: "1CHbjoEJ5NRH-SSM-mGMxLxNtn5tMVh5W", title: "2027 숭실대 학종가이드북.pdf", type: "PDF" }
+];
